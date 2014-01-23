cf6d11980b6bf5203676fe34c01ec97eb68caa30
darinrogers <darinrogers@hotmail.com>
2014-01-23 14:23:06 -0800

    MongoClient connect timeout much shorter

--- Framework/Mappers/MongoMapper.php
+++ Framework/Mappers/MongoMapper.php
@@ -86,7 +86,7 @@ abstract class MongoMapper
     	if ($this->_client == null) {
     		$this->_client = new \MongoClient(
     			$this->getConnectionString(), 
-    			array('connectTimeoutMS' => 1000)
+    			array('connectTimeoutMS' => 250) // so it won't hang for one minute if a secondary is down
 			);
     	}
     	
